ccb0265e19a56345fe430723978e7efbe9ee4c1e
keviner99 <esp21007@byui.edu>
2021-10-29 18:25:31 -0500

    updated

---
+++ speed/game/director.py
@@ -0,0 +1,31 @@
+from game.score import Score 
+from game.points import Point 
+from game.input_service import InputService
+from game.words import Words
+
+class Director: 
+    """
+    A code template for a person who directs the game. The responsibility of
+    this class of objects is to control the sequence of play.
+    """
+
+    def __init__(self, input_service):
+        """
+        The class constructor.
+        """
+
+    def start_game(self):
+        pass 
+
+
+    def _input_service(self):
+        pass
+
+
+    def _do_points(self):
+        pass
+
+
+    def _get_score(self):
+        pass 
+
